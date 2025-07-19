77f10f1fdce696472233698e9b051f64ee8f5175
Rayssa <rayssarrsilva1@gmail.com>
2025-07-19 17:37:16 -0300

    criação arquivos pro desafio 2 da etapa 6

---
+++ logica/etapa6/desafio2/codigo.py
@@ -0,0 +1,7 @@
+"""
+Desafio 2 - Sistema de Análise de Exames Médicos
+🎯 Objetivo
+Simular um sistema que processa e analisa dados de exames laboratoriais com base em listas paralelas, retornando estatísticas e alertas clínicos.
+
+"""
+
