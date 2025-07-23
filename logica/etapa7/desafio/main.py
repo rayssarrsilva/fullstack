a845d7e5a1f842abe4ca34dab1104388e825928a
Rayssa <rayssarrsilva1@gmail.com>
2025-07-23 19:42:40 -0300

    criação do arquivo main da etapa 7 para orquestrar as funções dos arquivos externos

--- logica/etapa7/desafio/main.py
+++ logica/etapa7/desafio/main.py
@@ -1,1 +1,10 @@
-# Orquestra o sistema
+# Orquestra o sistema (Título, Score, Prioridade, Tempo aberto, Setor, Status + Estatísticas finais com base na função)
+from entrada import entrada_chamados_tecnicos
+from processamento import *
+
+entrada = entrada_chamados_tecnicos()
+calculo_score = calcular_score(entrada())
+score_atribuido = atribuir_scores(calculo_score, entrada)
+ordenacao_chamados = ordenar_chamados(score_atribuido)
+status_classificado = classificar_status(ordenacao_chamados)
+estatistica = analisar_estatisticas(ordenacao_chamados)
