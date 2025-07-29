7bdfe3ed900e9dd9aa3bec19c87aa1fb60bf53eb
Rayssa <rayssarrsilva1@gmail.com>
2025-07-29 12:18:40 -0300

    criação das funoes, logica condicional do sistema de diagnostico de impressora

---
+++ logica/etapa8/desafio/diagnostico.py
@@ -0,0 +1,67 @@
+#entrada de dados do sistema de Diagnóstico de Impressora com Falha
+import logging
+import pdb
+
+logging.basicConfig(level=logging.DEBUG)
+
+def verificar_funcionamento_impressora():
+    while True:
+        try:
+            impressora_ligada = str(input("A impressora está ligada (sim/nao)? ")).strip()
+            print(f"DEBUG: valor inserido na entrada da funcao 'verificar_funcionamento_impressora': {impressora_ligada}")
+            if impressora_ligada.lower() == "sim":
+                print(f"DEBUG: entrando no bloco impressora_ligada.lower() == sim")
+                print("A impressora está funcionando")
+                break
+            elif impressora_ligada.lower() == "nao":
+                logging.debug(f"DEBUG: entrando no bloco impressora_ligada.lower() == nao")
+                print("Impressora desligada. Verifique o cabo de energia")
+                break
+            else:
+                raise ValueError
+        except ValueError as e:
+            logging.error(f"DEBUG: erro encontrado {e}")
+            print("Você deve digitar sim ou nao")
+
+def verificar_impressao():
+    while True:
+        try:
+            papel_suficiente = input("Há papel suficiente na impresssora(sim/nao)? ").strip()
+            print(f"DEBUG: valor inserido na entrada da funcao 'verificar_impressao': {papel_suficiente}")
+            if papel_suficiente.lower(): #verifica se foi inserido algo (todo retorno de input é str)
+                if papel_suficiente == "nao":
+                    print(f"DEBUG: entrando no papel_suficiente == nao")
+                    print("Coloque mais papel na impressora, para realizar impressões")
+                    break
+                elif papel_suficiente == "sim":
+                    print(f"DEBUG: entrando no bloco papel_suficiente == sim")
+                    print("A impressora está pronta para imprimir")
+                    break
+            else:
+                raise ValueError
+        except ValueError as e :
+            print(f"DEBUG: erro encontrado {e}")
+            print("Você deve digitar sim ou nao")
+
+def verificar_visor():
+    while True:
+        try:
+            visor_erro = str(input("O visor mostra erro (sim/nao)? ")).strip()
+            print(f"DEBUG: valor inserido na entrada da funcao 'verificar_visor': {visor_erro}")
+            if visor_erro.lower() in ["sim", "nao"]:
+                if visor_erro == "sim":
+                    print(f"DEBUG: entrando no bloco visor_erro == sim")
+                    print("O chamado tecnico foi efetivado para o concerto do visor, aguarde....")
+                elif visor_erro == "nao":
+                    print(f"DEBUG: entrando no bloco visor_erro == nao")
+                    print("O visor da impressora está funcionando corretamente")
+                break
+            else:
+                raise ValueError
+        except ValueError as e:
+            print(f"DEBUG: erro encontrado {e}")
+            print("Você deve digitar sim ou nao")
+
+verificar_funcionamento_impressora()
+verificar_impressao()
+verificar_visor()
