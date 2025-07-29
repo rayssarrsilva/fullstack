8f2048df6d2881931d0386379b076b8dc56a2b44
Rayssa <rayssarrsilva1@gmail.com>
2025-07-29 16:14:19 -0300

    alteração noe arquivo teste pra test (boas praticas)

--- logica/etapa8/desafio/test.py
+++ logica/etapa8/desafio/test.py
@@ -1,13 +1,14 @@
-import diagnostico
+from desafio.diagnostico import verificar_visor
 import pytest
 
 def test_output_verificarVisor(monkeypatch, capsys):
     #entrada do usuario simulada como sim
     monkeypatch.setattr("builtins.input", lambda _: "sim")
 
-    diagnostico.verificar_visor()
+    verificar_visor()
 
     captura_impressao = capsys.readouterr()
 
     #verifica se é a mensagem que apareceu
     assert "Há erro no visor. O chamado tecnico foi efetivado para o concerto do visor, aguarde...." in captura_impressao.out
+
