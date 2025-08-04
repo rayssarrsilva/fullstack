c35cab9aa796f6d0cb4bd170439ab0fb2a811135
Rayssa <rayssarrsilva1@gmail.com>
2025-08-04 18:41:34 -0300

    modularizaçãdo código no arquivo conexao pra nao deletar as tabelas ao abrir o script diversas vezes

--- logica/etapa10/desafio10/banco/conexao.py
+++ logica/etapa10/desafio10/banco/conexao.py
@@ -6,18 +6,18 @@ import os
 base_dir = os.path.dirname(os.path.abspath(__file__)) #base do arquivo atual
 caminho_banco = os.path.join(base_dir, '..', 'data', 'banco.db') #volta um nivel de pasta, acessa a pata data e depois o arquivo
 
-
 dados = sqlite3.connect(caminho_banco)
 cursor = dados.cursor()
 
-with open(os.path.join(base_dir, 'script.sql'), mode='r') as script:
-    cursor.executescript(script.read())
+def rodar_script(): #sempre que for rodado deleta as tabelas antigas e cria novas
+    with open(os.path.join(base_dir, 'script.sql'), mode='r') as script:
+        cursor.executescript(script.read())
 
-dados.commit() #Confirma e grava no disco a última vez que deu commit.
-dados.close() #Fecha a conexão com o banco de dados.
+    dados.commit() #Confirma e grava no disco a última vez que deu commit.
+    dados.close() #Fecha a conexão com o banco de dados.
 
 def conectar():
     base_dir = os.path.dirname(os.path.abspath(__file__)) #se posiciona no arquivo atual
     caminho_banco = os.path.join(base_dir, '..', 'data', 'banco.db') #leva ao banco de dados
+    
     return sqlite3.connect(caminho_banco) #conecta no caminho do banco de dados
-
