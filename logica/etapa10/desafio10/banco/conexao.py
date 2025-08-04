bda4b804c177159a6b905c4f0e8aa8c9e2a29c76
Rayssa <rayssarrsilva1@gmail.com>
2025-08-04 17:03:56 -0300

    conexao do script com o banco de dados

---
+++ logica/etapa10/desafio10/banco/conexao.py
@@ -0,0 +1,17 @@
+# Cria conexão com SQLite
+
+import sqlite3 #módulo sqlite3 embutido no Python
+import os 
+
+base_dir = os.path.dirname(os.path.abspath(__file__)) #base do arquivo atual
+caminho_banco = os.path.join(base_dir, '..', 'data', 'banco.db') #volta um nivel de pasta, acessa a pata data e depois o arquivo
+
+
+dados = sqlite3.connect(caminho_banco)
+cursor = dados.cursor()
+
+with open(os.path.join(base_dir, 'script.sql'), mode='r') as script:
+    cursor.executescript(script.read())
+
+dados.commit() #Confirma e grava no disco a última vez que deu commit.
+dados.close() #Fecha a conexão com o banco de dados.
