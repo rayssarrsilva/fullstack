7ffe78102cf5733a403357e58334227b5cc54920
Rayssa <rayssarrsilva1@gmail.com>
2025-08-04 18:08:14 -0300

    funcao criada para ao ser chamada conectar ao banco de dados

--- logica/etapa10/desafio10/banco/conexao.py
+++ logica/etapa10/desafio10/banco/conexao.py
@@ -15,3 +15,9 @@ with open(os.path.join(base_dir, 'script.sql'), mode='r') as script:
 
 dados.commit() #Confirma e grava no disco a última vez que deu commit.
 dados.close() #Fecha a conexão com o banco de dados.
+
+def conectar():
+    base_dir = os.path.dirname(os.path.abspath(__file__)) #se posiciona no arquivo atual
+    caminho_banco = os.path.join(base_dir, '..', 'data', 'banco.db') #leva ao banco de dados
+    return sqlite3.connect(caminho_banco) #conecta no caminho do banco de dados
+
