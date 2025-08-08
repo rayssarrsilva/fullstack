f8b84e03b801233b62ff7823e6e2356454a12582
Rayssa <rayssarrsilva1@gmail.com>
2025-08-08 17:44:48 -0300

    coloca a restrição NOT NULL na tabela filmes no campo titulo para uso na função em opracoes

--- logica2/etapa10/desafio10/banco/script.sql
+++ logica2/etapa10/desafio10/banco/script.sql
@@ -10,7 +10,7 @@ CREATE TABLE clientes (
 
 CREATE TABLE filmes (
     id_filme INTEGER PRIMARY KEY AUTOINCREMENT,
-    titulo VARCHAR(100),
+    titulo VARCHAR(100) NOT NULL,
     categoria VARCHAR(50) NOT NULL,
     preco_aluguel DECIMAL(10, 2)
 );
